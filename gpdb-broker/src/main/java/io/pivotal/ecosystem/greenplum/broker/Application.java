b7ff1af119d3be33dc1daedeaad7fed16c74f288
Michael Wright <mwright@macbook-pro-4.attlocal.net>
2018-06-16 10:51:23 -0400

    working build

--- gpdb-broker/src/main/java/io/pivotal/ecosystem/greenplum/broker/Application.java
+++ gpdb-broker/src/main/java/io/pivotal/ecosystem/greenplum/broker/Application.java
@@ -17,8 +17,12 @@
 package io.pivotal.ecosystem.greenplum.broker;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableAutoConfiguration
 @SpringBootApplication
 public class Application {
 
